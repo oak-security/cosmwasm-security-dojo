21dab311fc4a1c457efcfe2a6886245b6cbded5b
jcr_oaksec <108589472+jcr-oaksec@users.noreply.github.com>
2023-07-27 11:57:05 +0200

    Ch4 exploit reworked

---
+++ challenges/04-nft/src/tests/mod.rs
@@ -0,0 +1,10 @@
+mod exploit;
+mod unittests;
+
+pub const ADMIN: &str = "admin";
+pub const USER1: &str = "user1";
+pub const NFT1: &str = "awesome NFT 1";
+pub const USER2: &str = "user2";
+pub const NFT2: &str = "awesome NFT 2";
+pub const USER3: &str = "user3";
+pub const NFT3: &str = "awesome NFT 3";
